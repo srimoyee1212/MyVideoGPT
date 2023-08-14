48a87c7d85285096627b6110639f26d6b89cf274
srimoyee1212 <srimoyeem1@gmail.com>
2023-08-13 20:33:46 -0400

    Conversational buffer memory

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 os.environ['OPENAI_API_KEY'] = mykey
 st.title('Video-Link GPT')
 prompt = st.text_input('Enter your prompt')
+
+memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 
 title_template = PromptTemplate(input_variables=['topic'], template='write me a youtube video title about {topic}')
 script_template = PromptTemplate(input_variables=['title'], template='write me a youtube video script on this title: {title}')
@@ -22,4 +25,7 @@ if prompt:
     response = sequential_chain({'topic':prompt})
     st.write(response['title'])
     st.write(response['script'])
+
+    with st.expander('Message History'):
+        st.info(memory.buffer)
    
