d747069a4fc6b902d3efdbc049ba5f7a944e7cb6
srimoyee1212 <srimoyeem1@gmail.com>
2023-08-13 20:37:29 -0400

    resolved issues

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 title_template = PromptTemplate(input_variables=['topic'], template='write me a youtube video title about {topic}')
 script_template = PromptTemplate(input_variables=['title'], template='write me a youtube video script on this title: {title}')
 
+llm = OpenAI(temperature=0.9)
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title')
 script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
 sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title', 'script'], verbose=True)
 
-llm = OpenAI(temperature=0.9)
+
 
 if prompt:
     response = sequential_chain({'topic':prompt})
